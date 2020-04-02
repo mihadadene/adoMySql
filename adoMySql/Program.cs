7b734e84ec3eaf87a613d308945d8c10b0acc4d2
mihadadene <mi.ihadadene@gmail.com>
2020-04-01 22:27:18 -0400

    first commit

---
+++ adoMySql/Program.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adoMySql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //connexion vers la BD north
+            //chaine de connection
+            string cs = "server = localhost;user = root; " + "database = northwindmysql; port = 3306";
+
+            //objet de connexion
+
+            MySqlConnection connection = new MySqlConnection(cs);
+            try
+            {
+                Console.WriteLine("Connection sur MySql....");
+                connection.Open();
+
+                //cree une requet
+                string query = "select ContactName, CompanyName from customers";
+
+                //preparer l'excution de la requet
+                MySqlCommand sql = new MySqlCommand(query, connection);
+
+                //Récuperer le curseur
+                MySqlDataReader reader = sql.ExecuteReader();
+
+                //parcourir le curseur
+                while (reader.Read())
+                {
+                    string nom, company;
+                    nom = (string)reader[0];
+                    company = (string)reader[1];
+                    Console.WriteLine("nom: {0} - compangnie: {1}", nom, company);
+                    //Console.ReadKey();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Clone();
+            }
+            }
+        }
+    }
+
